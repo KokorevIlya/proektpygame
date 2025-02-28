54ee718b3bf0f20a124f1340c1bd752bf81003eb
Ilya <developer@yandex.ru>
2025-02-28 19:07:24 +0300

    релиз

--- map_game_textur.py
+++ map_game_textur.py
@@ -302,7 +302,7 @@ def start_screen():
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
